fecfd6f1f24509482d184f4701c5d85a30d03007
Andrei Yangabishev <waffleboot@gmail.com>
2021-07-22 04:30:23 +0300

    http

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+const message = "hello"
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(message))
+	})
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatalf("server failed to start: %v", err)
+	}
 }
